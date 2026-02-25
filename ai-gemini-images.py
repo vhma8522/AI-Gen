8e524260068d83b1ea728f3583e76c7a6f8c03da
VHMA <vhma.workdev@gmail.com>
2026-02-24 19:48:49 -0600

    First code to gemini API

---
+++ ai-gemini-images.py
@@ -0,0 +1,18 @@
+from google import genai
+from google.genai import types
+from PIL import Image #Import the Pillow library for image handling
+
+client = genai.Client(api_key="AIzaSyCsLVel40vRDF1Sl_kouXJuycesve1L9ic")
+
+prompt = ("Create a picture of a nano banana dish in a fancy restaurant with a Gemini theme")
+response = client.models.generate_content(
+    model="gemini-2.5-flash-image",
+    contents=[prompt],
+)
+
+for part in response.parts:
+    if part.text is not None:
+        print(part.text)
+    elif part.inline_data is not None:
+        image = part.as_image()
+        image.save("generated_image.png")
---
+++ ai-gemini.py
@@ -0,0 +1,17 @@
+from google import genai
+
+## iMPORT ENV
+import os
+from dotenv import load_dotenv
+
+# Load the variables from the .env file into the system environment
+load_dotenv()
+
+# The client gets the API key from the environment variable `GEMINI_API_KEY`.
+#client = genai.Client(api_key="AIzaSyCsLVel40vRDF1Sl_kouXJuycesve1L9ic")
+client = genai.Client()
+
+response = client.models.generate_content(
+    model="gemini-3-flash-preview", contents="Explain how AI works in a few words"
+)
+print(response.text)
